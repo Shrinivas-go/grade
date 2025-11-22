0943942d8801727ad73988ec347011db9dc76bd2
Shrinivas <shrinivas4927@gmail.com>
2025-11-22 13:01:33 +0530

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,26 +1,48 @@
 import sys
-if len(sys.argv) != 6:
-  print("Usage: python grade.py <sub1> <sub2> <sub3> <sub4> <sub5>")
-  sys.exit(1)
-sub1 = int(sys.argv[1])
-sub2 = int(sys.argv[2])
-sub3 = int(sys.argv[3])
-sub4 = int(sys.argv[4])
-sub5 = int(sys.argv[5])
-avgMarks = (sub1 + sub2 + sub3 + sub4 + sub5)/ 5
-grade = "null"
-if avgMarks >= 90 :
-  grade = "A"
-elif avgMarks >= 80:
-  grade = "B"
-elif avgMarks >= 70:
-  grade = "C"
-elif avgMarks >= 40:
-  grade = "D"
-elif avgMarks < 40:
-  grade = "Fail"
-else:
-  print("invalid")
+if len(sys.argv) == 7:
+  script_name = sys.argv[0] 
+  sub1 = int(sys.argv[1])
+  sub2 = int(sys.argv[2])
+  sub3 = int(sys.argv[3])
+  sub4 = int(sys.argv[4])
+  sub5 = int(sys.argv[5])
+  avgMarks = (sub1 + sub2 + sub3 + sub4 + sub5)/ 5
+  grade = "null"
+  if avgMarks >= 90 :
+    grade = "A"
+  elif avgMarks >= 80:
+    grade = "B"
+  elif avgMarks >= 70:
+    grade = "C"
+  elif avgMarks >= 40:
+    grade = "D"
+  elif avgMarks < 40:
+    grade = "Fail"
+  else:
+    print("invalid")
 
-print("Average marks: ", avgMarks)
-print("Grade: ", grade)
+  print("Average marks: ", avgMarks)
+  print("Grade: ", grade)
+else:
+  script_name = sys.argv[0] 
+  sub1 = 78
+  sub2 = 90
+  sub3 = 89
+  sub4 = 67
+  sub5 = 45
+  avgMarks = (sub1 + sub2 + sub3 + sub4 + sub5)/ 5
+  grade = "null"
+  if avgMarks >= 90 :
+    grade = "A"
+  elif avgMarks >= 80:
+    grade = "B"
+  elif avgMarks >= 70:
+    grade = "C"
+  elif avgMarks >= 40:
+    grade = "D"
+  elif avgMarks < 40:
+    grade = "Fail"
+  else:
+    print("invalid")
+  print("Average marks: ", avgMarks)
+  print("Grade: ", grade)
